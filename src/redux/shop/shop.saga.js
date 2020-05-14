1e326bc41c8deb0e2aa3d5604227026944ed5d30
Carey Best <cbest@indeed.com>
2020-05-14 00:14:37 -0500

    fetchCollectionsAsync setup for saga actions

--- src/redux/shop/shop.saga.js
+++ src/redux/shop/shop.saga.js
@@ -1,4 +1,14 @@
-import { takeEvery } from 'redux-saga/effects';
+import { takeEvery, call, put } from 'redux-saga/effects';
+
+import {
+  firestore,
+  convertCollectionsSnapshotToMap
+} from '../../firebase/firebase.utils';
+
+import {
+  fetchCollectionsSuccess,
+  fetchCollectionsFailure
+} from './shop.actions';
 
 import ShopActionTypes from './shop.types';
 
@@ -6,6 +16,19 @@ import ShopActionTypes from './shop.types';
 // – video 182, Redux Saga, 5:30 mark
 export function* fetchCollectionsAsync() {
   yield console.log('I am firing!');
+
+  try {
+    const collectionRef = firestore.collection('collections');
+    const snapshot = yield collectionRef.get();
+    const collectionsMap =
+      yield call(
+        convertCollectionsSnapshotToMap,
+        snapshot
+      );
+    yield put(fetchCollectionsSuccess(collectionsMap));
+  } catch (error) {
+    yield put(fetchCollectionsFailure(error.message));
+  }
 }
 
 export function* fetchCollectionsStart() {
