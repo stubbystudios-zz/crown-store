1d0022cceff234fc0a49c45ad0f6db80a12023be
Carey Best <cbest@indeed.com>
2020-05-20 16:16:03 -0500

    Stripe payments setup

--- src/pages/Homepage/Homepage.styles.jsx
+++
@@ -1,8 +0,0 @@
-import styled from 'styled-components';
-
-export const HomepageContainer = styled.div`
-  display: block;
-  flex-direction: column;
-  align-items: center;
-  padding: 20px 80px;
-`;
