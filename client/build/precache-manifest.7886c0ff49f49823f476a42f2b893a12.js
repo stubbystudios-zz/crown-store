eba5908738ef1c6459653b0e3cb23f1b10a99c27
Carey Best <cbest@indeed.com>
2020-05-20 18:48:34 -0500

    Heroku deployment

---
+++ client/build/precache-manifest.7886c0ff49f49823f476a42f2b893a12.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10b99ff8d65b9c30a24e6f99a1305cf1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "19733330406fa0d62b73",
+    "url": "/static/css/main.fd62adec.chunk.css"
+  },
+  {
+    "revision": "059bfadc5395d97ef014",
+    "url": "/static/js/2.f6b371a1.chunk.js"
+  },
+  {
+    "revision": "f9b3e87900b5f3843f2b97c9b5b24d1e",
+    "url": "/static/js/2.f6b371a1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "19733330406fa0d62b73",
+    "url": "/static/js/main.f95f8b38.chunk.js"
+  },
+  {
+    "revision": "62d82d29d19ace6699e0",
+    "url": "/static/js/runtime-main.b690b024.js"
+  },
+  {
+    "revision": "ed28accb7dd58a0469bff983503ff07e",
+    "url": "/static/media/logo.ed28accb.svg"
+  },
+  {
+    "revision": "30f0707118fb960665491424c115ac44",
+    "url": "/static/media/shopping-icon.30f07071.svg"
+  }
+]);
